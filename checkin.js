3a014f14d71284fb22eef9f0d28154c15cf94017
Sergio Duran <sergio@tiene.info>
2021-10-13 10:41:29 -0500

    Minor cleanup

--- checkin.js
+++ checkin.js
@@ -1,10 +1,16 @@
 const process = require('process');
 const fs = require("fs");
-const dayjs = require('dayjs')
 
 const Worky = require('./worky')
 
 const username = process.argv[2]
+const entry_date = process.argv[3]
+const exit_date = process.argv[4]
+
+if (exit_date == undefined) {
+  console.error("Error: exit date undefined");
+  usage()
+}
 
 function usage() {
   console.error("Usage: node checkin.js {username} {entry_date} {exit_date} < password.txt")
@@ -18,21 +24,14 @@ if (username == undefined) {
 
 let password
 try {
-  password = fs.readFileSync(0).toString().trim(); // STDIN_FILENO = 0
+  password = fs.readFileSync(0).toString().trim();
 } catch (error) {
   console.error("Error: could not read the password from the standard input")
   usage()
 }
 
-const entry_date = process.argv[3]
-const exit_date = process.argv[4]
 
-if (exit_date == undefined) {
-  console.error("Error: exit date undefined");
-  usage()
-}
-
-worky = new Worky()
+const worky = new Worky()
 worky.login(username, password).then(async () => {
   await worky.checkin(entry_date)
   await worky.checkout(exit_date)
