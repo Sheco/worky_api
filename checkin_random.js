454fa774ef6c7e18297ec15e97cad6780820bb3a
Sergio Duran <sergio@tiene.info>
2021-10-14 09:58:45 -0500

    Add help text to the checkin_random script

--- checkin_random.js
+++ checkin_random.js
@@ -9,7 +9,16 @@ let entry_date = today.format("YYYY-MM-DD HH:mm")
 today.$H = 17
 today.$m = Math.floor(Math.random()*30)
 let exit_date = today.format("YYYY-MM-DD HH:mm")
+function usage() {
+  console.error("Usage: node checkin_random.js {username}")
+  console.error("The password is read from the standard input, so you can either")
+  console.error("type it or pipe it in like node checkin_random.js {username} < password.txt")
+  process.exit(1)
+}
 const username = process.argv[2]
+if (username == undefined) {
+  usage()
+}
 
 child_process.fork("checkin.js", [username, entry_date, exit_date], {
   'stdio': [
