b0709206db6b7422aadd66a3811625cc5f342b29
Sergio Duran <sergio@tiene.info>
2022-03-15 11:11:40 -0600

    Define common date format string

--- checkin_random.mjs
+++ checkin_random.mjs
@@ -2,13 +2,15 @@
 import * as child_process from 'child_process'
 import dayjs from 'dayjs'
 
+let dateformat = 'YYYY-MM-DD HH:mm'
+
 let entry_date = dayjs().hour(9).minute(0)
   .subtract(Math.random()*30, 'minute')
-  .format('YYYY-MM-DD HH:mm')
+  .format(dateformat)
 
 let exit_date = dayjs().hour(17).minute(0)
   .add(Math.random()*30, 'minute')
-  .format('YYYY-MM-DD HH:mm')
+  .format(dateformat)
 
 child_process.fork("run.mjs", [
   '--checkin', entry_date, 
