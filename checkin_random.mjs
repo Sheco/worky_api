0b2d9f18d2f10e59b24767d58a434697423b9036
Sergio Duran <sergio@tiene.info>
2022-03-15 11:11:25 -0600

    Remove unused usage function

--- checkin_random.mjs
+++ checkin_random.mjs
@@ -10,15 +10,6 @@ let exit_date = dayjs().hour(17).minute(0)
   .add(Math.random()*30, 'minute')
   .format('YYYY-MM-DD HH:mm')
 
-function usage() {
-  console.error(`Usage: node checkin_random.js
-The credentials are read from the .env file, for example:
-USER=someuser@company.com
-PASS=somepass
-  `)
-  process.exit(1)
-}
-
 child_process.fork("run.mjs", [
   '--checkin', entry_date, 
   '--checkout', exit_date], {
