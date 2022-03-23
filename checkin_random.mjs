2d1780063e166eb7f79cd250261aa96ff2f9ebb5
Sergio Duran <sergio@tiene.info>
2022-03-22 18:33:37 -0600

    Update the checkin_random script

--- checkin_random.mjs
+++ checkin_random.mjs
@@ -4,8 +4,7 @@ import { fork } from 'child_process'
 fork("run.mjs", [
   '--today',
   '--checkin', '9:00', 
-  '--checkin_early',
   '--checkout', '17:00',
-  '--checkout_late'
+  '--random', '10',
 ])
 
